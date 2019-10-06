15976d7714e6bbecca1e016b84ecdab962a388b5
Anton Grygoriev <agrygoriev@Academic.RRC.ca>
2019-10-06 17:08:12 -0500

    implemented routes for Book views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
+  get '/books', to: 'books#index'
+  get '/books/:id', to: 'books#show', id: /\d+/
+
+  root to: 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
