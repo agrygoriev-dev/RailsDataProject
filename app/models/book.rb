9ad939f4ea2e9b36ee22a89f4f898958932bdef2
Anton Grygoriev <agrygoriev@Academic.RRC.ca>
2019-10-05 21:01:55 -0500

    Added associations to models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class Book < ApplicationRecord
+  has_many :book_authors
+  has_many :book_genres
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
 class Genre < ApplicationRecord
+  has_many :book_genres
 end
