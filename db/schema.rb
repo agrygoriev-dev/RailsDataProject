f1d3e75226d16f03dab36617413fcc943206a874
Anton Grygoriev <agrygoriev@Academic.RRC.ca>
2019-10-05 20:32:12 -0500

    Created Author model and ran the migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_06_012823) do
+ActiveRecord::Schema.define(version: 2019_10_06_013125) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
