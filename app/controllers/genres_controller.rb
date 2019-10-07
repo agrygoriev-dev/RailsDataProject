56154a34222011fe27a8803833f318e0daad6e9d
Anton Grygoriev <agrygoriev@Academic.RRC.ca>
2019-10-06 21:44:38 -0500

    Updated genres controller

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class GenresController < ApplicationController
   def index
+    @genres = Genre.order(:name)
   end
 
   def show
+    @genre = Genre.find(params[:id])
   end
 end
